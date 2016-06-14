ca433878b1d12f6b9d1e4921865b636c14b4cdc4
pwjablonski <pwjablonski@gmail.com>
2016-06-14 14:03:28 -0400

    Done.

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -8,3 +8,42 @@ import Foundation
 
  */
 
+func stringForDeliLine(names:Array<String>) -> String {
+    var string = "The line is"
+    var count = 1
+    
+    if names.count == 0 {
+        string += " currently empty."
+    }
+    else {
+        string += ":"
+        for name in names{
+            string += "\n\(count). \(name)"
+            count += 1
+        }
+    }
+
+    
+    return string
+}
+
+func addName(name:String, toDeliLine:Array<String>) -> Array<String>{
+    var newLine = toDeliLine
+    newLine.append(name)
+   
+    print("Welcome you are \(newLine.count)")
+    return newLine
+}
+
+func serveNextCustomerInDeliLine(deliLine:Array<String>)->Array<String>{
+    var newLine = deliLine
+    if newLine.count != 0 {
+        newLine.removeFirst()
+        print("\(newLine[0]) its your turn")
+        return newLine
+    }
+    else {
+        return newLine
+    }
+}
+
