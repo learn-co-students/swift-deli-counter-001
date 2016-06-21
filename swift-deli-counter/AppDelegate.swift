09810170fc59959947be27124563f0ce0a4ecb66
John Richardson <richardsonjm@gmail.com>
2016-06-21 11:23:24 -0400

    DeliLine function

--- swift-deli-counter/AppDelegate.swift
+++ swift-deli-counter/AppDelegate.swift
@@ -11,7 +11,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        // let iosAncestors = ["Al", "Chris", "Zach"]
+        let iosAncestors = ["Al", "Chris", "Zach"]
+
+        print(stringForDeliLine(iosAncestors))
+        print(stringForDeliLine([]))
         
         // Do not alter
         return true  //
--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -2,9 +2,14 @@
 
 import Foundation
 
-/**
+func stringForDeliLine(lineArray: [String]) -> String {
+    if lineArray.count == 0 {
+        return "The line is currently empty."
+    }
 
- * Write your functions here!
-
- */
-
+    var deliLine = "The Line is:"
+    for i in 0 ..< lineArray.count {
+        deliLine = deliLine + "\n\(i + 1). \(lineArray[i])"
+    }
+    return deliLine
+}
